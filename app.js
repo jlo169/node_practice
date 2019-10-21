21f84588cc26c88830585d501e648db091971b7c
Jonathan <kakashi169@gmail.com>
2019-10-21 01:17:40 +0000

    installed upper-case module through npm and made a string upper case

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const uc = require('upper-case');
+
+http.createServer( (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc('Pug World'));
+    res.end();
+}).listen(8080);
